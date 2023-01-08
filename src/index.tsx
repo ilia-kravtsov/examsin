009440fc4163541b09bc79a2166713f491e975bb
_ilia_kravtsov <kravtsov.i.s@mail.ru>
2023-01-08 19:09:04 +0400

    solve_7

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+type UserWalletType = {
+    title: string
+    amount: number
+}
+type UserWalletPropsType = {
+    wallet: UserWalletType
+}
+export const UserMoney: React.FC<UserWalletPropsType> = (props: UserWalletPropsType) => {
+
+    const wallets = [
+        {title: 'bitcoin', amount: 1},
+        {title: '$', amount: 100}
+    ]
+
+    return <div>
+        <UserWallet wallet={wallets[0]} />
+        <UserWallet wallet={wallets[1]} />
+    </div>
+}
+
+export const UserWallet: React.FC<UserWalletPropsType> = (props) => {
+    return <div>title: {props.wallet.title}, amount: {props.wallet.amount}</div>
+}
